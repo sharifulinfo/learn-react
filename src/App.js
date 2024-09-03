b7f0f3046ba7587073ffcc44586ba6c7b00b0616
sharif <shariful.info55@gmail.com>
2024-09-03 22:47:02 +0600

    added note

--- src/App.js
+++ src/App.js
@@ -2,20 +2,39 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [counter , setCounter] = useState(20);
-  const increassCounter = () => {
-    setCounter(counter + 1);
+  
+  const [noteTitle,setNoteTitle] = useState("");
+  const [notes,setNotes] = useState([]);
+
+  const addNote = (e) => {
+    e.preventDefault();
+    if(noteTitle.trim() === ""){
+      return alert("please enver valid note");
+    }
+    const n = {
+      id : Date.now(),
+      title : noteTitle
+    }
+    setNotes([n,...notes]);
+    console.log(notes);
   }
-  const decressCounter = () => {
-    setCounter(counter - 1);
+
+  const chengeNoteTitle = (e) => {
+    setNoteTitle(e.target.value);
   }
 
   return (
     <div className="App">
-     <h2>Counter : {counter}</h2>
-     <button onClick={increassCounter}>Incress</button>
-     <button onClick={decressCounter}>Decress</button>
+      <form onSubmit={addNote}>
+        <input type="text" value={noteTitle} onChange={chengeNoteTitle} />
+        <button type="submit">Save Note</button> 
+      </form>
       
+      <ul>
+        {notes.map((n)=>(
+          <li key={n.id}>{n.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
